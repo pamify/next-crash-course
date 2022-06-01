a723f598503493a88bdc49a8718e78d8908753c4
Pamela Jung <72463342+pamify@users.noreply.github.com>
2022-06-01 16:49:50 -0400

    create about page, Layout component and update styles

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f9955205b8480a60"; }
+/******/ 		__webpack_require__.h = function() { return "054c5cd224aaa837"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
