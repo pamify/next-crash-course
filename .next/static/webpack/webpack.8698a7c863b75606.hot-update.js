b6404d2a9b67c8f2c5803b2521a5892aabf0d822
Pamela Jung <72463342+pamify@users.noreply.github.com>
2022-06-01 18:03:17 -0400

    create nested routing for articles

--- .next/static/webpack/webpack.8698a7c863b75606.hot-update.js
+++ .next/static/webpack/webpack.8698a7c863b75606.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "3027cb773f0498f8"; }
+/******/ 	__webpack_require__.h = function() { return "02940ae5e8730245"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
