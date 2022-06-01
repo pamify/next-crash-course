27835ceb1f99430a12f668036a6788cab319e796
Pamela Jung <72463342+pamify@users.noreply.github.com>
2022-06-01 16:54:39 -0400

    add classes to Header

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "054c5cd224aaa837"; }
+/******/ 		__webpack_require__.h = function() { return "38b9f0fca8945308"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
