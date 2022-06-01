12b4899039e468faa5962d5a4a5553a2b3bf4951
Pamela Jung <72463342+pamify@users.noreply.github.com>
2022-06-01 17:39:23 -0400

    create ArticleList and ArticleItem components and update styles

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3027cb773f0498f8"; }
+/******/ 		__webpack_require__.h = function() { return "d64c6375b307de12"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
